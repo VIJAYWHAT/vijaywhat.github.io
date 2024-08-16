92ec43b3bb09a9f14c8754e17e590a93c181401c
VijayWhat <vijayrv042@gmail.com>
2024-08-16 16:39:03 +0530

    changes

--- content/js/script.js
+++ content/js/script.js
@@ -1,24 +1,21 @@
-const container = document.querySelector(".container"),
-      pwShowHide = document.querySelectorAll(".showHidePw"),
-      pwFields = document.querySelectorAll(".password");
+const pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwField = document.querySelector("#password"); // Select the password input field directly
 
-    //   js code to show/hide password and change icon
-    pwShowHide.forEach(eyeIcon =>{
-        eyeIcon.addEventListener("click", ()=>{
-            pwFields.forEach(pwField =>{
-                if(pwField.type ==="password"){
-                    pwField.type = "text";
+// JS code to show/hide password and change icon
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        if (pwField.type === "password") {
+            pwField.type = "text";
 
-                    pwShowHide.forEach(icon =>{
-                        icon.classList.replace("uil-eye-slash", "uil-eye");
-                    })
-                }else{
-                    pwField.type = "password";
+            pwShowHide.forEach(icon => {
+                icon.classList.replace("uil-eye-slash", "uil-eye");
+            });
+        } else {
+            pwField.type = "password";
 
-                    pwShowHide.forEach(icon =>{
-                        icon.classList.replace("uil-eye", "uil-eye-slash");
-                    })
-                }
-            }) 
-        })
-    })
+            pwShowHide.forEach(icon => {
+                icon.classList.replace("uil-eye", "uil-eye-slash");
+            });
+        }
+    });
+});
